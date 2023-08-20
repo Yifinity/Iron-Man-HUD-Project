14f27922ffb527a56238185dca13475e28bed3f3
Yifan Jiang <yifan99.jiang@gmail.com>
2023-08-20 14:47:47 -0400

    add CPU temperature

--- Features/CPU.py
+++ Features/CPU.py
@@ -1,28 +1,17 @@
 # Get CPU Info
-
-import Feature
-
+from Feature import *
 class Clock(Feature):
     def __init__(self):
         # Inherit nessisary fonts
         super().__init__(self)
-        self.timePos = (10, 75)
-        self.fpsPos = (10, self.timePos[1] + 20)
+        # Put on right side
+        self.tempPos = (700, 50)
 
     def onStep(self):
-        self.now = datetime.now() # Current Date and Time.
-        # Format time 
-        self.date_time = self.now.strftime("%B %d, %Y | %I:%M%p")
+        # Give us our temperature
+        self.cpuTemp = CPUTemperature()
 
-        # FPS Calculation
-        self.timeNow = time.time()
-        self.fps = 1 / (self.timeNow - self.startTime)
-        self.startTime = self.timeNow
-    
     def redraw(self, img):
-        cv2.putText(img, self.date_time, self.timePos, self.font, 0.5,
+        cv2.putText(img, self.cpuTemp, self.tempPos, self.font, 0.5,
                      self.HUDText, 1)
-        
-        cv2.putText(img, ("FPS: "+ str(math.floor(self.fps))), self.fpsPos,
-                     self.font, 0.5, self.HUDText, 1)      
-        
+         
