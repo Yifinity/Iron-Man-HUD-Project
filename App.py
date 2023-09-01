eaee799630d347d6090554b19c6d7bb7b93785e1
Yifan Jiang <yifan99.jiang@gmail.com>
2023-08-31 21:48:18 -0400

    create an app object

---
+++ App.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from PIL import Image
+
+from Screen import *
+from Camera import *
+
+def main():
+    # Create our objects to run 
+    userCamera = Camera()
+    userScreen = Screen()
+
+    while(True):
+        frame = userCamera.getFrame()
+        userScreen.onStep()
+        userScreen.redrawAll(frame)
+
+
+        # Check if user presses ESC to exit loop
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+        else:
+            userScreen.keyPress(k)
+    
+    # Outside the loop, we've ended the stream. 
+    userCamera.terminateStream()
+    exit()
+
+main()
--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import os
-import numpy as np
-from PIL import Image
-
-from Screen import *
-from Camera import *
+from App import *
 
 def main():
-    # Create our objects to run 
-    userCamera = Camera()
-    userScreen = Screen()
+    # Create a new app instance 
+    app = App()
 
     while(True):
-        frame = userCamera.getFrame()
-        userScreen.onStep()
-        userScreen.redrawAll(frame)
+        app.onStep()
+        app.redrawAll()
+        app.onKeyPress()
 
-
-        # Check if user presses ESC to exit loop
-        k = cv2.waitKey(1)
-        if k == 27:
-            break
-        else:
-            userScreen.keyPress(k)
-    
-    # Outside the loop, we've ended the stream. 
-    userCamera.terminateStream()
     exit()
 
 main()
